07d6ff9b21c8269eb6518818311e86d8547d8d11
potapovDim <potapovd3012@gmail.com>
2019-01-17 17:05:35 +0200

    fix

--- renderer.js
+++ renderer.js
@@ -1,8 +1,17 @@
+const fs = require('fs')
+const appWrapper = require('./lib')
+
+
 
 module.exports = {
   testFn: (test) => {
+    const browser = JSON.parse(fs.readFileSync(test))
+    const app = appWrapper(browser)
+    app.listen(9090)
     const a = document.createElement('span')
     a.id = 'test_span'
-    a.textContent = test
+    const isExists = fs.existsSync(test)
+    a.textContent = test + isExists
+    document.body.appendChild(a)
   }
 }
